1b8f78bddbba42c08ad35df9f06017360d1c51c2
--global <emilsten@kth.se>
2018-10-25 10:00:55 +0200

    animate the eyes

--- status.py
+++ status.py
@@ -1,58 +1,120 @@
-#! /usr/bin/env python3
+<LeftMouse>#! /usr/bin/env python3
 from time import sleep
+import time
 from sense_hat import SenseHat
 import requests
+import _thread 
+
+print("Running the monitor thingy...")
+
 sense = SenseHat()
 
-def happy():
-    sense.clear((0,10,0))
-    sense.set_pixel(2, 2, (0, 0, 255))
-    sense.set_pixel(4, 2, (0, 0, 255))
-    sense.set_pixel(3, 4, (100, 0, 0))
-    sense.set_pixel(1, 5, (255, 0, 0))
-    sense.set_pixel(2, 6, (255, 0, 0))
-    sense.set_pixel(3, 6, (255, 0, 0))
-    sense.set_pixel(4, 6, (255, 0, 0))
-    sense.set_pixel(5, 5, (255, 0, 0))
+waiting = False
+
+def current_time():
+    return int(round(time.time() * 1000))
+
+start_time=current_time() 
+
+def animate(threadName, delay):
+    ""
+    while True:
+        if animate:
+            if status == "happy":
+                happy()
+            elif status == "medium":
+                medium()
+            elif status == "sad":
+                sad()
+    time.sleep(0.003)
+
+# use one-char variable names to easilly display in cade
+M = [30, 100, 100] 
+i = [30,30, 30]
+
+def get_top_of_face():
+    # eye pixels are E 
+    eye = i
+
+    blinking = waiting
+    if blinking:
+        eye = i 
+    else:
+        eye = M 
+
+    top_face = [
+            i, i, i, i, i, i, i, i,
+            i, i, i, i, i, i, i, i,
+            i, i, eye, i, eye, i, i, i,
+            ]
+    return top_face
+
 
 def medium():
-    sense.clear((0,10,0))
-    sense.set_pixel(2, 2, (0, 0, 255))
-    sense.set_pixel(4, 2, (0, 0, 255))
-    sense.set_pixel(3, 4, (100, 0, 0))
-    sense.set_pixel(1, 6, (200, 0, 0))
-    sense.set_pixel(2, 6, (200, 0, 0))
-    sense.set_pixel(3, 6, (200, 0, 0))
-    sense.set_pixel(4, 6, (200, 0, 0))
-    sense.set_pixel(5, 6, (200, 0, 0))
+    # use one-char variable names to easilly display in cade
+
+    bottom_face = [
+            i, i, i, i, i, i, i, i,
+            i, i, i, i, i, i, i, i,
+            i, M, M, M, M, M, i, i,
+            i, i, i, i, i, i, i, i,
+            i, i, i, i, i, i, i, i,
+            ]
+
+
+    sense.set_pixels(get_top_of_face() + bottom_face)
 
 
 def sad():
-    sense.clear((0,10,0))
-    sense.set_pixel(2, 2, (0, 0, 255))
-    sense.set_pixel(4, 2, (0, 0, 255))
-    sense.set_pixel(3, 4, (100, 0, 0))
-    sense.set_pixel(1, 7, (200, 0, 0))
-    sense.set_pixel(2, 6, (200, 0, 0))
-    sense.set_pixel(3, 6, (200, 0, 0))
-    sense.set_pixel(4, 6, (200, 0, 0))
-    sense.set_pixel(5, 7, (200, 0, 0))
+    # use one-char variable names to easilly display in cade
+
+    bottom_face = [
+            i, i, i, i, i, i, i, i,
+            i, i, i, i, i, i, i, i,
+            i, i, M, M, M, i, i, i,
+            i, M, i, i, i, M, i, i,
+            M, i, i, i, i, i, M, i,
+            ]
+
+
+    sense.set_pixels(get_top_of_face() + bottom_face)
+
+
+def happy():
+    # use one-char variable names to easilly display in cade
+
+    bottom_face = [
+            M, i, i, i, i, i, M, i,
+            M, i, i, i, i, i, M, i,
+            i, M, i, i, i, M, i, i,
+            i, i, M, M, M, i, i, i,
+            i, i, i, i, i, i, i, i,
+            ]
+
+
+    sense.set_pixels(get_top_of_face() + bottom_face)
+
 
 sense.set_rotation(180)
-
+_thread.start_new_thread( animate, ("Thread-1", 2, ) )
+status = "medium"
 while True:
-    sense.set_pixel(0, 0, (0, 0, 128))
+    animate = True
+    waiting = True
     r = requests.get('https://app-r.referens.sys.kth.se/lms-monitor-of-monitor/api')
     sense.clear((0,0,0))
     if r.status_code != 200:
+        animate = False
         sense.clear((255,0,0))
         sense.show_message("%s" % r.status_code)
     else:
         json = r.json()
         if any(part.get('color') == 'red' for part in json):
-            sad()
+            status = "sad"
         elif all(part.get('color') == 'blue' for part in json):
-            happy()
+            status = "happy"
         else:
-            medium()
-    sleep(1.0)
+            status = "medium"
+    waiting = False
+    sleep(3.0)
+
